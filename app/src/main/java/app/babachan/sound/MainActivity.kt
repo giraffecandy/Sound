282859b4ec895d58825cd134189e58b0c97ea82c
lifeistech <[umemomo534@gmail.com]>
2020-09-05 00:49:27 +0900

    終了 1時間いろいろやってみて教科書通りに

--- app/src/main/java/app/babachan/sound/MainActivity.kt
+++ app/src/main/java/app/babachan/sound/MainActivity.kt
@@ -1,8 +1,9 @@
 package app.babachan.sound
 
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,12 +12,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val drumSound = MediaPlayer.create(this, R.raw.drum_sound)
+        val guitarSound = MediaPlayer.create(this, R.raw.guitar_sound)
+        val pianoSound = MediaPlayer.create(this, R.raw.piano_sound)
 
-        drumImage.setOnClickListener {
-            drumSound.isPlaying
+        drumImage.setOnTouchListener { view, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                drumSound.seekTo(0)
+                drumSound.start()
+                drumImage.setImageResource(R.drawable.drum_playing_image)
+            } else if (event.action == MotionEvent.ACTION_UP) {
+                drumImage.setImageResource(R.drawable.drum_image)
+            }
+            true
         }
 
-        if (drumSound.isPlaying && drumImage.setOnClickListener)
+        guitarImage.setOnTouchListener { view, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                guitarSound.seekTo(0)
+                guitarSound.start()
+                guitarImage.setImageResource(R.drawable.guitar_playing_image)
+            } else if (event.action == MotionEvent.ACTION_UP) {
+                guitarImage.setImageResource(R.drawable.guitar_image)
+            }
+            true
+        }
+
+        pianoImage.setOnTouchListener { view, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                pianoSound.seekTo(0)
+                pianoSound.start()
+                pianoImage.setImageResource(R.drawable.piano_playing_image)
+            } else if (event.action == MotionEvent.ACTION_UP) {
+                pianoImage.setImageResource(R.drawable.piano_image)
+            }
+            true
+        }
 
     }
 }
